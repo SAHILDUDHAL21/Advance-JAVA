f0190f18171c8b9487966d7dac87313d43c27752
Sahil Dudhal <129080375+SAHILDUDHAL21@users.noreply.github.com>
2024-12-26 14:37:27 +0530

    26-12-24

---
+++ src/ArrayList1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ArrayList1
+{
+        public static void main(String[] args)
+        {
+                ArrayList al=new ArrayList();
+
+                System.out.println("Size : "+al.size());
+                System.out.println("Check Empty : "+al.isEmpty());
+                System.out.println("Count Total Elements : "+al.stream().count());
+                al.add(11);
+                al.add(44); //number add karu shakto
+               // float f=new Float(4.6);
+                al.add(33.6f); // pointed value pn add karu shakto
+                // ata string
+                al.add("chala ja bhos***ke");
+
+                System.out.println("Count Total Elements : "+al.stream().count());
+                System.out.println(al);
+        }
+}
