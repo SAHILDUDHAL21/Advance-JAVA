2d62c3bc0a2927488cc3fafa293dd8b4037ef505
Sahil Dudhal <129080375+SAHILDUDHAL21@users.noreply.github.com>
2024-12-27 04:35:59 +0530

    27-12-24

--- src/Arraylist2.java
+++ src/Arraylist2.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+
 public class Arraylist2
 {
         public static void main(String[] args)
         {
-                
+                boolean b;
+                ArrayList al=new ArrayList(5);
+                al.add(11);
+                al.add(44);
+                al.add('h');
+                al.add(1,"altered");
+                System.out.println("remove by index :"+al.remove(0));
+               // System.out.println("Remove by value : "+al.remove(44);
+                // cant reomve by index by driectly printn
+                b= (boolean) al.remove("44");// nahi chalatr bc
+               System.out.println(al);
         }
 }
